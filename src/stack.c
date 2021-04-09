198daa2f904eb3f0d2424bf836eabe6272dcc77d
Fletcher T. Penney <fletcher@fletcherpenney.net>
2021-04-09 09:33:06 -0400

    UPDATED: Update astyle config

--- src/stack.c
+++ src/stack.c
@@ -18,19 +18,19 @@
 
 
 	MIT License
-	
+
 	Copyright (c) 2016-2020 Fletcher T. Penney
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -151,7 +151,7 @@ void * stack_peek_index(stack * s, size_t index) {
 #ifdef TEST
 void Test_stack_push_pop(CuTest * tc) {
 	stack * s = stack_new(1);
-	
+
 	char * s1 = "foo";
 	char * s2 = "bar";
 	char * s3 = "bat";
